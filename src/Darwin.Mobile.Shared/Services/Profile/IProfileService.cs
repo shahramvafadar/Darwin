b4b8cf2296db5f8cec152a12993d48d5ae7b5843
Shahram - DEV <dev@loyan.de>
2026-03-18 13:27:23 +0100

    Disable scanner action when operator lacks permissions

--- src/Darwin.Mobile.Shared/Services/Profile/IProfileService.cs
+++ src/Darwin.Mobile.Shared/Services/Profile/IProfileService.cs
@@ -1,12 +1,13 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Darwin.Contracts.Profile;
+using Darwin.Shared.Results;
 
 namespace Darwin.Mobile.Shared.Services.Profile
 {
     public interface IProfileService
     {
         Task<CustomerProfile?> GetMeAsync(CancellationToken ct);
-        Task<bool> UpdateMeAsync(CustomerProfile profile, CancellationToken ct);
+        Task<Result> UpdateMeAsync(CustomerProfile profile, CancellationToken ct);
     }
 }
